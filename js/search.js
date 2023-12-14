2605212a67e5ee5d8c6c687ac939dd3cd4c3022d
SnIvanova <1snizhanaivanova@gmail.com>
2023-12-14 12:02:24 +0100

    mockups

---
+++ js/search.js
@@ -0,0 +1,5 @@
+/*Cercate di creare anche una funzione di ricerca. Questo API possiede un endpoint DEDICATO!
+Potete creare una pagina a parte o mostrare tutto sulla homepage.
+Parametro: query
+Endpoint: https://striveschool-api.herokuapp.com/api/deezer/search?q={query}
+Example: https://striveschool-api.herokuapp.com/api/deezer/search?q=queen*/
