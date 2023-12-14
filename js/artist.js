80ad1f98517c1cde6e3c786b279ee724e98a348a
SnIvanova <1snizhanaivanova@gmail.com>
2023-12-14 12:12:34 +0100

    Update artist.js

--- js/artist.js
+++ js/artist.js
@@ -1,2 +1,19 @@
 //Parameter: artist id
 //Endpoint: https://striveschool-api.herokuapp.com/api/deezer/artist/{id}
+const artistUrl = 'https://striveschool-api.herokuapp.com/api/deezer/artist/';
+
+let artist = new URLSearchParams(location.search).get('artist');
+
+function fetchArtist(artist){
+
+    fetch(`${artistUrl}${artist}`)
+    .then(response => response.json())
+    .then(data => {
+        showsong(data);
+        fetchSongs(data);
+    })
+    .catch(error => {
+        console.error(error);
+    });
+
+}
