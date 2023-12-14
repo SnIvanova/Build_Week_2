78ce944f6dac42a3b0aa6425d0ef2be86e8e1e71
Francesco Zatti <francesco.zatti98gmail.com>
2023-12-14 18:09:43 +0100

    consegna footer di fine giornata

--- footer.js
+++
@@ -1,9 +0,0 @@
- document.addEventListener('DOMContentLoaded', function() {
-    function replaceHeartIcon() {
-        const heartIcon = document.querySelector('.bi-heart');
-        const filledHeartIcon = document.createElement('i');
-        filledHeartIcon.classList.add('bi', 'bi-heart-fill');
-        
-        heartIcon.parentNode.replaceChild(filledHeartIcon, heartIcon);
-      }
-  });
