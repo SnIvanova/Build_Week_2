9a0102df491ccc590d853a1d35e3ac465412b2b0
SnIvanova <1snizhanaivanova@gmail.com>
2023-12-15 11:21:33 +0100

    problem

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
 //Parametro: query
 //Endpoint: https://striveschool-api.herokuapp.com/api/deezer/search?q={query}
 
+//amici
+document.addEventListener('DOMContentLoaded', () => {
+    let closeButton = document.querySelector('#closeBtn');
+    closeButton.addEventListener('click', () => {
+        let section = document.querySelector('#sectionAmici');
+        section.style.display = 'none';
+    })
+})
 
+// footer
 document.addEventListener('DOMContentLoaded', function() {
     function replaceHeartIcon() {
         const heartIcon = document.querySelector('.bi-heart');
@@ -11,3 +20,30 @@ document.addEventListener('DOMContentLoaded', function() {
         heartIcon.parentNode.replaceChild(filledHeartIcon, heartIcon);
       }
   });
+
+// Playlist Colonna SX
+
+let playlistUL = document.querySelector(".overflow-auto");
+
+let arrayPlaylist = ["Playlist bella", "I grandi succesi"]; // Contenuto ricavato dalle API
+
+console.log(arrayPlaylist)
+
+ /* document.addEventListener('DOMContentLoaded', () => {}) */
+
+function createPlaylist (a) {
+
+    console.log(playlistUL);
+
+    for (let i=0; i<a.length; i++) {
+        
+        let playLi = document.createElement("li");
+        playLi.className = "text-white-50 fs-6 list-unstyled";
+        playLi.innerText = a[i];
+        playlistUL.appendChild(playLi);
+
+        console.log(arrayPlaylist);
+    }
+}
+
+createPlaylist(arrayPlaylist);
